e6090f63738e6eebafb4d9e266098d55176cee8b
Luciano Cataffo <cataffo.luciano.dev@gmail.com>
2019-07-10 02:01:37 -0300

    mas models a cata

--- models/user.js
+++ models/user.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
         email: {
             type: DataTypes.STRING,
             allowNull: false,
+            isUnique: true,
             validate: {
                 isEmail: true
             }
